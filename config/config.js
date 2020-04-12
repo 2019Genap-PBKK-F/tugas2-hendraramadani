989b92a40504ab2760cdbbcca587bdcf664a555a
Hendra Ramadani <hendraramadani14@gmail.com>
2020-04-12 07:29:49 +0700

    Changes brooo

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const dbconf = {
-    user: 'su',
+    user: 'sa',
     password: 'SaSa1212',
     server: '10.199.13.253',
     database: 'nrp05111740000055',
--- server.js
+++ server.js
@@ -23,46 +23,101 @@ app.get("/", function (req, res) {
 });
 
 //select all
-app.get("/api/mahasiswa", function (req, res) {
-   var query = "SELECT * FROM [mahasiswa]";
+app.get("/api/datadasar", function (req, res) {
+   var query = "SELECT * FROM [datadasar]";
    console.log('select data');
    execute.execqr(res, query, null);
 });
 
+app.get("/api/capaianunit", function (req, res) {
+   var query = "SELECT * FROM [capaian_unit]";
+   console.log('select data');
+   execute.execqr(res, query, null);
+});
+
+app.get("/api/unit", function (req, res) {
+   var query = "SELECT * FROM [unit]";
+   console.log('select data');
+   execute.execqr(res, query, null);
+});
+
+
+//////////////////////////// KATEGORI UNIT ///////////////////////////////////////////
+
+//select
+app.get("/api/kategoriunit", function (req, res) {
+   var query = "SELECT * FROM [kategoriunit]";
+   console.log('select kategoriunit');
+   execute.execqr(res, query, null);
+});
+
 //delete
-app.delete('/api/mahasiswa/:id', function (req, res) {
+app.delete('/api/kategoriunit/:id', function (req, res) {
    var param = [
       { name: 'id', sqltype: sql.Int, value: req.params.id }
     ]
-   var query = "DELETE FROM [mahasiswa] WHERE id=@id";
-   console.log('delete data');
+   var query = "DELETE FROM [kategoriunit] WHERE id=@id";
+   console.log('delete kategoriunit');
    execute.execqr(res, query, param);
 })
 
 //insert
-app.post('/api/mahasiswa',function(req,res){
-    var query = "INSERT INTO [mahasiswa] (nrp,nama,jk,lahir,foto,aktif,angkatan) VALUES ('', '', '', '', '', '', '');"
-    console.log('insert data');
+app.post('/api/kategoriunit',function(req,res){
+    var query = "INSERT INTO [kategoriunit] (nama) VALUES ('');"
+    console.log('insert kategoriunit');
     execute.execqr(res, query, null);
 })
 
 //update
-app.put('/api/mahasiswa/:id',function(req,res){
+app.put('/api/kategoriunit/:id',function(req,res){
    var param = [
       { name: 'id', sqltype: sql.Int, value: req.params.id },
-      { name: 'nrp', sqltype: sql.Char, value: req.body.nrp },
-      { name: 'nama', sqltype: sql.VarChar, value: req.body.nama },
-      { name: 'angkatan', sqltype: sql.Int, value: req.body.angkatan },
-      { name: 'jk', sqltype: sql.Char, value: req.body.jk },
-      { name: 'lahir', sqltype: sql.Char, value: req.body.lahir },
-      { name: 'foto', sqltype: sql.VarChar, value: req.body.foto },
-      { name: 'aktif', sqltype: sql.Bit, value: req.body.aktif }
+      { name: 'nama', sqltype: sql.VarChar, value: req.body.nama }
     ]
-    var query = "UPDATE [mahasiswa] SET nrp = @nrp, nama = @nama, jk = @jk, lahir = @lahir, foto = @foto, aktif = @aktif, angkatan = @angkatan WHERE id = @id;"
-    console.log('update data');
+    var query = "UPDATE [kategoriunit] SET nama = @nama WHERE id = @id;"
+    console.log('update kategoriunit');
     execute.execqr(res, query, param);
 })
 
+////////////////////////////////////////////////////////////////////////////
+//////////////////////////// DATA DASAR ////////////////////////////////////
+
+//select
+app.get("/api/datadasar", function (req, res) {
+   var query = "SELECT * FROM [datadasar]";
+   console.log('select datadasar');
+   execute.execqr(res, query, null);
+});
+
+//delete
+app.delete('/api/datadasar/:id', function (req, res) {
+   var param = [
+      { name: 'id', sqltype: sql.Int, value: req.params.id }
+    ]
+   var query = "DELETE FROM [datadasar] WHERE id=@id";
+   console.log('delete datadasar');
+   execute.execqr(res, query, param);
+})
+
+//insert
+app.post('/api/datadasar',function(req,res){
+    var query = "INSERT INTO [datadasar] (nama) VALUES ('');"
+    console.log('insert datadasar');
+    execute.execqr(res, query, null);
+})
+
+//update
+app.put('/api/datadasar/:id',function(req,res){
+   var param = [
+      { name: 'id', sqltype: sql.Int, value: req.params.id },
+      { name: 'nama', sqltype: sql.VarChar, value: req.body.nama }
+    ]
+    var query = "UPDATE [datadasar] SET nama = @nama WHERE id = @id;"
+    console.log('update datadasar');
+    execute.execqr(res, query, param);
+})
+
+////////////////////////////////////////////////////////////////////////////
 app.listen(8010, function () {
    console.log('Listen on port 8010')
 })
